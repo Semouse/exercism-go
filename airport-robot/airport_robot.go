e95a6c998b28d168962f2e0815f3e6be44d2e7b5
lborok <tratos149@gmail.com>
2025-06-08 09:45:17 +0300

    Added Airport Robot solution

---
+++ airport-robot/airport_robot.go
@@ -0,0 +1,34 @@
+package airportrobot
+
+import "fmt"
+
+type Greeter interface {
+	LanguageName() string
+	Greet(name string) string
+}
+
+type Italian struct {
+}
+
+func (italian Italian) LanguageName() string {
+	return "Italian"
+}
+
+func (italian Italian) Greet(name string) string {
+	return fmt.Sprintf("Ciao %s!", name)
+}
+
+type Portuguese struct {
+}
+
+func (portuguese Portuguese) LanguageName() string {
+	return "Portuguese"
+}
+
+func (portuguese Portuguese) Greet(name string) string {
+	return fmt.Sprintf("Olá %s!", name)
+}
+
+func SayHello(name string, greeter Greeter) string {
+	return fmt.Sprintf("I can speak %s: %s", greeter.LanguageName(), greeter.Greet(name))
+}
